8e84f3e27978a39dc319b46ffa4b45683b451f96
YMGYM <good_day1234@naver.com>
2021-01-22 16:12:01 +0900

    20200122 add random kuji

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [Click, setClick] = useState(false);
+  const [Kuji, setKuji] = useState("")
+
+  const handleClick = () => {
+    if(Click === false){
+      // Math.random(); 0~0.999 까지의 난수를 반환
+      const random = Math.ceil(Math.random()*5)
+      switch(random){
+        case 1:
+          setKuji("대길이에욤");
+          break
+        case 2:
+          setKuji("중길이에욤");
+          break;
+        case 3:
+          setKuji("말길이에욤");
+          break;
+        case 4:
+          setKuji("흉이에욤");
+          break;
+        case 5:
+          setKuji("대흉이에욤");
+          break;
+      }
+    }
+    setClick(true);
+
+
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign: "center"}}>Happy "YOMI KUZI"</h1>
+      <h3 style={{textAlign: "center"}}>그림을 클릭해 올해의 운세를 뽑아보세요!</h3>
+      <div style = {{width: '99vw', height: "40vh", display: "flex", justifyContent: "center", marginTop: "5vh"}}
+      onClick= {handleClick}>
+        <img style = {{width: '99vw', height: "40vh"}} src="omikuji.png" />
+      </div>
+      
+      {
+        Click ? (
+        <div style={{marginTop: "5vh", marginLeft: "10vw", marginRight: "10vw", textAlign: "center"}}>
+          <div className="paperIn">
+            <div className="paper">
+              <p>{Kuji}</p>
+            </div>
+          </div>
+        </div> )
+        : ""
+      }
     </div>
   );
 }
